d935242610fa7901487a67249b7db539e061a24f
Matt Jensen <mattj65816@gmail.com>
2024-04-09 06:29:52 -0500

    Build to static HTML.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+/** @type {(string) => import("next").NextConfig} */
+export default async phase => {
+  const config = {
+    env: {
+      BUILD_TIMESTAMP: new Date().toISOString(),
+    },
+  };
+  if ("phase-development-server" !== phase) {
+    config.output = "export";
+  }
+  return config;
+};
