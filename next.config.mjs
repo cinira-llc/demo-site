cab4cfde3e94bd70bc2dfb135d0eeabca94531a7
Matt Jensen <mattj65816@gmail.com>
2024-04-13 06:30:06 -0500

    Empty landing page.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
+import _ from "lodash";
+import { exec } from "child_process";
+
 /** @type {(string) => import("next").NextConfig} */
 export default async phase => {
   const config = {
     env: {
+      BUILD_SOURCE: await new Promise((resolve, reject) => {
+        exec("git describe --abbrev=11 --always --dirty --first-parent --long --tags",
+          (err, stdout) => {
+            if (err) {
+              resolve("unknown");
+            }
+            resolve(_.trim(stdout));
+          });
+      }),
       BUILD_TIMESTAMP: new Date().toISOString(),
     },
   };
